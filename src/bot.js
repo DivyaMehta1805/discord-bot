81bbe653e6d7c94f680f82ecaaad483112ca3178
brainhacker509 <93784062+brainhacker509@users.noreply.github.com>
2022-01-12 20:23:28 +0530

    Add files via upload

---
+++ src/bot.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+console.log(process.env.DISCORDJS_BOTT_TOKEN);
+const { Client }= require('discord.js');
+const client = new Client();
+const prefix="$";
+
+client.login(process.env.DISCORDJS_BOTT_TOKEN);
+client.on('message',(message)=>{
+    if(message.author.bot)
+    return;
+    if(message.content=='radhika aur prayag chutiya')
+    {
+        message.channel.send('radhika aur prayag chutiya');
+    }
+    if(message.content=='bye')
+    {
+        message.channel.send('I would miss you');
+    }
+    if(message.content.startsWith(prefix))
+    {
+        const [cmdnme,...args]=message.content
+        .substring(prefix.length)
+        .trim()
+        .split(/\s+/);
+        if(cmdnme=='kick')
+        {
+            message.channel.send("user kicked");
+        }
+    }
+})
